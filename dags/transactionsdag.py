8ef21bf3af1f8db3432106cb3c20e3e74cbdc4a3
Paulo Aguiar <aguiarlapaulo@gmail.com>
2024-10-30 16:44:39 -0300

    feat-another error

--- dags/transactionsdag.py
+++ dags/transactionsdag.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+import os
 
 # Default settings for the DAG
 default_args = {
@@ -20,23 +21,28 @@ with DAG(
     catchup=False,
 ) as dag:
 
+
+    print = BashOperator(
+        task_id='print',
+        bash_command='pwd',
+    )
     # Task to run staging models
     run_dbt_staging = BashOperator(
         task_id='dbt_run_staging',
-        bash_command='cd /home/user/dbt-transactions/transactions && dbt run --select staging',
+        bash_command='cd /usr/local/airflow/transactions && dbt run --select staging',
     )
 
     # Task to run intermediate models
     run_dbt_intermediate = BashOperator(
         task_id='dbt_run_intermediate',
-        bash_command='cd /home/user/dbt-transactions/transactions && dbt run --select intermediate',
+        bash_command='cd C:/Users/user/Python/dbt-transactions/transactions && dbt run --select intermediate',
     )
 
     # Task to run marts models
     run_dbt_marts = BashOperator(
         task_id='dbt_run_marts',
-        bash_command='cd /home/user/dbt-transactions/transactions && dbt run --select marts',
+        bash_command='cd C:/Users/user/Python/dbt-transactions/transactions && dbt run --select marts',
     )
 
     # Defining the order of task execution
-    run_dbt_staging >> run_dbt_intermediate >> run_dbt_marts
+    print >> run_dbt_staging >> run_dbt_intermediate >> run_dbt_marts
